d5974c282454ce7d0d38606408b0c2d93d8936d1
Ritvik5 <sharma.ritvik56@gmail.com>
2023-06-18 22:23:45 +0530

    [Ritvik]Add: Generics Custom class for stack

--- GenericsDataStructures/Program.cs
+++ GenericsDataStructures/Program.cs
@@ -1,18 +1,28 @@
 ﻿namespace GenericsDataStructures
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Generics Data Structures!!!\n");
 
-            LinkedList<int> list = new LinkedList<int>();
+            //-- Generics Linked List
 
-            list.Add(56);
-            list.Add(30);
-            list.Add(70);
+            //LinkedList<int> list = new LinkedList<int>();
 
-            list.Display();
+            //list.Add(56);
+            //list.Add(30);
+            //list.Add(70);
+
+            //list.Display();
+
+            //-- Generics Stack
+
+            GenericsStack<int> stack = new GenericsStack<int>();
+            stack.Push(70);
+            stack.Push(30);
+            stack.Push(56);
+            stack.Display();
         }
     }
 }
