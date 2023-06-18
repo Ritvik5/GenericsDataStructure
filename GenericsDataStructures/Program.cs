2c0ed8ae430a306891a9405d79ef0f900d63c113
Ritvik5 <sharma.ritvik56@gmail.com>
2023-06-18 22:14:10 +0530

    [Ritvik]Add: Generics custom class linked list

--- GenericsDataStructures/Program.cs
+++ GenericsDataStructures/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Generics Data Structures!!!");
+            Console.WriteLine("Welcome to Generics Data Structures!!!\n");
+
+            LinkedList<int> list = new LinkedList<int>();
+
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
+
+            list.Display();
         }
     }
 }
